d56b270b5dd1597906ba7f9194abb108c006202e
chenxiangdong <ohayo_cxd@126.com>
2015-01-19 19:19:19 +0800

    hello world class

---
+++ testmodel/src/com/cxd/system/MyFirstClass.java
@@ -0,0 +1,12 @@
+package com.cxd.system;
+
+/**
+ * descrip: 第一个测试类
+ * Created by chenxd on 2015/1/19.
+ */
+public class MyFirstClass {
+
+	public static void main(String[] args) throws Exception {
+		System.out.println("hello world");
+	}
+}
